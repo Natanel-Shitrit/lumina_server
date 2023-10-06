54cc5acc20faef0176da95f0cd267d3a44d3af05
Natanel Shitrit <65548905+Natanel-Shitrit@users.noreply.github.com>
2023-10-06 16:13:39 +0300

    Refactoring

--- lumina/lumina_server.py
+++ lumina/lumina_server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-import os, sys, argparse, logging, signal, threading
+from abc import ABC, abstractmethod
+import sys, argparse, logging, signal, threading
+import pprint
 
 from socketserver import ThreadingMixIn, TCPServer, BaseRequestHandler
 import socket, ssl
@@ -26,110 +28,70 @@ class LuminaRequestHandler(BaseRequestHandler):
         self.database = server.database
         super().__init__(request, client_address, server)
 
-
-    def sendMessage(self, code, **kwargs):
-        self.logger.debug(f"sending RPC Packet (code = {code}, data={kwargs}")
-
+    def send_rpc_message(self, code, **kwargs):
         data = rpc_message_build(code, **kwargs)
+        self.logger.debug(f"sending RPC Packet ({code=}, {kwargs=}")
         self.request.send(data)
 
-    def recvMessage(self):
+    def recv_rpc_message(self):
         packet, message = rpc_message_parse(self.request)
-        self.logger.debug(f"got new RPC Packet (code = {packet.code}, data={message}")
+        self.logger.debug(f"got new RPC Packet ({packet.code=}, {message=}")
         return packet, message
 
     def handle(self):
-
-        #
-        # Get first RPC packet (RPC_HELO)
-        #
-
-        packet, message = self.recvMessage()
+        packet, message = self.recv_rpc_message()
 
         if packet.code != RPC_TYPE.RPC_HELO:
-            self.sendMessage(RPC_TYPE.RPC_NOTIFY, message = 'Expected helo')
+            self.send_rpc_message(RPC_TYPE.RPC_NOTIFY, message = 'Expected helo')
             return
 
-        if self.server.check_client(message):
-            self.sendMessage(RPC_TYPE.RPC_OK)
-        else:
-            self.sendMessage(RPC_TYPE.RPC_NOTIFY, message = 'Invalid license')
+        if self.server.is_blacklisted(message):
+            self.send_rpc_message(RPC_TYPE.RPC_FAIL, status = 0, message = 'You are black-listed!')
             return
+        
+        self.send_rpc_message(RPC_TYPE.RPC_OK)
 
-        #
-        # Receive and handle request command:
-        #
-        packet, message = self.recvMessage()
+        while self.handle_rpc_message():
+            pass
 
-        if packet.code == RPC_TYPE.PUSH_MD:
-            results = list()
-            for _, info in enumerate(message.funcInfos):
-                results.append(self.database.push(info))
+    def handle_rpc_message(self) ->  bool:
+        try:
+            packet, message = self.recv_rpc_message()
+        except:
+            self.logger.debug('connection has been closed.')
+            return False
+        
+        match packet.code:
+            case RPC_TYPE.PUSH_MD:
+                pprint.pprint(message)
+                self.send_rpc_message(RPC_TYPE.PUSH_MD_RESULT, resultsFlags=[self.database.push(info) for info in message.funcInfos])
+            case RPC_TYPE.PULL_MD:
+                signatures = [self.database.pull(sig) for sig in message.funcInfos]
+                self.send_rpc_message(
+                    RPC_TYPE.PULL_MD_RESULT,
+                    found=list(map(lambda v: int(v is not None), signatures)),
+                    results=list(filter(None, signatures))
+                )
+            case _:
+                self.logger.debug("[-] ERROR: message handler not implemented")
+                self.send_rpc_message(RPC_TYPE.RPC_NOTIFY, message = "Unknown command")
 
-            self.sendMessage(RPC_TYPE.PUSH_MD_RESULT, resultsFlags = results)
+        return True
 
-        elif packet.code == RPC_TYPE.PULL_MD:
-            found = list()
-            results = list()
-
-            for sig in message.funcInfos:
-                metadata = self.database.pull(sig)
-                if metadata:
-                    found.append(1)
-                    results.append(metadata)
-                else:
-                    found.append(0)
-
-            self.sendMessage(RPC_TYPE.PULL_MD_RESULT, found = found, results =results)
-
-        else:
-            self.logger("[-] ERROR: message handler not implemented")
-            self.sendMessage(RPC_TYPE.RPC_NOTIFY, message = "Unknown command")
-            #self.sendMessage(RPC_TYPE.RPC_FAIL, status = -1, message = "not implemented")
-
-        return
-
-class LuminaServer(ThreadingMixIn, TCPServer):
+class BaseLuminaServer(ABC, ThreadingMixIn, TCPServer):
     def __init__(self, database, config, logger, bind_and_activate=True):
         super().__init__((config.ip, config.port), LuminaRequestHandler, bind_and_activate)
         self.config = config
         self.database = database
         self.logger = logger
-        self.useTLS = False
-
-        if self.config.cert:
-            if self.config.cert_key is None:
-                raise ValueError("Missing certificate key argument")
-
-            self.useTLS = True
 
     def get_request(self):
-        client_socket, fromaddr = self.socket.accept()
+        conn, info = self.socket.accept()
 
-        self.logger.debug(f"new client {fromaddr[0]}:{fromaddr[1]}")
-        if not self.useTLS:
-            self.logger.debug("Starting plaintext session")
-            # extra check: make sure client does no try to initiate a TLS session (or parsing would hang)
-            data = client_socket.recv(3, socket.MSG_PEEK)
-            if data == b'\x16\x03\x01':
-                self.logger.error("TLS client HELLO detected on plaintext mode. Check IDA configuration and cert. Aborting")
-                client_socket.close()
-                raise OSError("NO TLS")
+        self.logger.debug(f"new client {info}")
+        self.handle_connection(conn, info)
 
-        if self.useTLS:
-            self.logger.debug("Starting TLS session")
-            try:
-                client_socket = ssl.wrap_socket(client_socket,
-                                                ssl_version = ssl.PROTOCOL_TLSv1_2,
-                                                server_side = True,
-                                                certfile=self.config.cert.name,
-                                                keyfile=self.config.cert_key.name)
-
-            except Exception:
-                self.logger.exception("TLS connection failed. Check IDA configuration and cert")
-                raise
-
-        return client_socket, fromaddr
+        return conn, info
 
     def shutdown(self, save=True):
         self.logger.info("Server stopped")
@@ -137,16 +99,54 @@ class LuminaServer(ThreadingMixIn, TCPServer):
         self.database.close(save=save)
 
     def serve_forever(self):
-        self.logger.info(f"Server started. Listening on {self.server_address[0]}:{self.server_address[1]} (TLS={'ON' if self.useTLS else 'OFF'})")
+        self.logger.info(f"Server started. Listening on {self.server_address[0]}:{self.server_address[1]} (TLS={'ON' if isinstance(self, TLSLuminaServer) else 'OFF'})")
         super().serve_forever()
 
-    def check_client(self, message):
+    def is_blacklisted(self, message):
         """
         Return True if user is authozied, else False
         """
         # check (message.hexrays_licence, message.hexrays_id, message.watermak, message.field_0x36)
-        self.logger.debug("RPC client accepted")
-        return True
+        # self.logger.debug("RPC client accepted")
+        self.logger.debug(f"Checking client {message.hexrays_id=}")
+        # Check if id is in whitelist
+        return message.hexrays_id in []
+
+    @abstractmethod
+    def handle_connection(self) -> None:
+        """
+        Handle request
+        """
+
+class PlainTextLuminaServer(BaseLuminaServer):
+    def handle_connection(self, conn, info):
+        if self.is_tls_connection(conn):
+            self.logger.error("TLS client HELLO detected on plaintext mode. Check IDA configuration and cert. Aborting")
+            conn.close()
+            raise OSError("NO TLS")
+
+        self.logger.debug("Starting plaintext session")
+    
+    @staticmethod
+    def is_tls_connection(conn: socket):
+        # https://tls12.xargs.org/#client-hello
+        return conn.recv(2, socket.MSG_PEEK) == b'\x16\x03'
+
+
+class TLSLuminaServer(BaseLuminaServer):
+    def handle_connection(self, conn, info) -> None:
+        self.logger.debug("Starting TLS session")
+        try:
+            conn = ssl.wrap_socket(conn,
+                                   ssl_version = ssl.PROTOCOL_TLSv1_2,
+                                   server_side = True,
+                                   certfile=self.config.cert.name,
+                                   keyfile=self.config.cert_key.name)
+
+        except Exception:
+            self.logger.exception("TLS connection failed. Check IDA configuration and cert")
+            raise
+
 
 def signal_handler(sig, frame, server):
     print('Ctrl+C caught. Exiting')
@@ -164,7 +164,7 @@ def main():
 
     # Parse command line
     parser = argparse.ArgumentParser()
-    parser.add_argument("db", type=argparse.FileType('a+'), default="", help="database file")
+    parser.add_argument("db", type=argparse.FileType('w+'), default="", help="database file")
     parser.add_argument("-i", "--ip", dest="ip", type=str, default="127.0.0.1", help="listening ip address (default: 127.0.0.1")
     parser.add_argument("-p", "--port", dest="port", type=int, default=4443, help="listening port (default: 4443")
     parser.add_argument("-c", "--cert", dest="cert", type=argparse.FileType('r'), default = None, help="proxy certfile (no cert means TLS OFF).")
@@ -172,13 +172,13 @@ def main():
     parser.add_argument("-l", "--log", dest="log_level", type=str, choices=["NOTSET", "DEBUG", "INFO", "WARNING"], default="INFO", help="log level bases on python logging value (default:info)")
     config = parser.parse_args()
 
-
     logger.setLevel(config.log_level)
 
     # create db & server
     database = LuminaDatabase(logger, config.db)
     TCPServer.allow_reuse_address = True
-    server = LuminaServer(database, config, logger)
+    start_tls_server = None not in (config.cert, config.cert_key)
+    server = (TLSLuminaServer if start_tls_server else PlainTextLuminaServer)(database, config, logger)
 
     # set ctrl-c handler
     signal.signal(signal.SIGINT, lambda sig,frame:signal_handler(sig, frame, server))
